870f0c7b88ee77a4d4223a96a6826ff11d92907e
Vincent Glendenning <notmutater@gmail.com>
2021-08-03 15:41:26 +0000

    Added uncommitted changes

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1 style='color:blue'>Hello World!</h1>"
+    return render_template("home.html")
+
+
+@app.route("/test")
+def test():
+    return render_template("test.html")
+
+@app.route("/magyka")
+def magyka():
+    return render_template("magyka.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
